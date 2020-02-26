75c6f71b4fbd51c412c58a1d1411938dc091ffb2
SafrazHakamali <safrazhakamali@gmail.com>
2020-02-26 22:40:35 +0800

    cant seem to get it to work

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import tweets from 'tweets'
+import tweets from 'tweets';
 
 class App extends React.Component {
   render() {
+  	const all = tweets.tweets;
     return (
       <div>
-        tweets
+        <Tweets hello={all}/>
       </div>
     );
-  }
-}
+  };
+};
+
+class Tweets extends React.Component {
+  render () {
+    let list = this.props.hello.map(tweet => {
+      return 
+      <Tweet name={tweet.user.name}>
+    });
+    return (
+      <div>{list}</div>
+    );
+  };
+};
+
+
+class Tweet extends React.Component {
+  render () {
+    return (
+      <div>
+        <div>{this.props.name}</div> 
+      </div>
+    );
+  };
+};
 
 const element = document.getElementById('app');
 
